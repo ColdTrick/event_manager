d8681d40672e9f911252c2857466a5d6b1d8415c
Jerome Bakker <jeabakker@coldtrick.com>
2012-01-30 10:56:27 +0100

    fixed: list not handling no events correctly

--- views/default/event_manager/list.php
+++ views/default/event_manager/list.php
@@ -1,5 +1,7 @@
 <?php 
     
-    echo "<div id='event_manager_event_listing'>";
-        echo elgg_view_entity_list($vars["entities"], $vars["count"], $vars["offset"], 10, false, true, false);
-    echo "</div>";
+	if(!empty($vars["entities"])){
+	    echo "<div id='event_manager_event_listing'>";
+		echo elgg_view_entity_list($vars["entities"], $vars["count"], $vars["offset"], 10, false, true, false);
+	    echo "</div>";
+	}
