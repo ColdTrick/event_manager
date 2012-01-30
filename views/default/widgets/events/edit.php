5112fa74877874fd8c25f221f76b020faff9eab9
Jerome Bakker <jeabakker@coldtrick.com>
2012-01-30 11:20:09 +0100

    fixed: several widget bugs

--- views/default/widgets/events/edit.php
+++ views/default/widgets/events/edit.php
@@ -8,31 +8,34 @@
 		elgg_extend_view("metatags", "event_manager/googlemapsjs");
 	}
 	
+	$widget = $vars["entity"];
+	
+	$num_display = (int) $widget->num_display;
+	$type_to_show = $widget->type_to_show;
+	
 	// set default value
-	if (!isset($vars['entity']->num_display)) {
-		$vars['entity']->num_display = 5;
+	if ($num_display < 1) {
+		$num_display = 5;
 	}
 	
-	if (!isset($vars['entity']->type_to_show)) {
-		$vars['entity']->type_to_show = 5;
-	}
 ?>
 <p>
 <?php 
 	echo elgg_echo('event_manager:widgets:events:numbertodisplay').':';
-	echo elgg_view('input/text', array('internalname' => 'params[num_display]', 'value' => $vars['entity']->num_display));
+	echo elgg_view('input/text', array('internalname' => 'params[num_display]', 'value' => $num_display));
 ?>
 </p>
 <p>
  <?php
 
-if(in_array(get_context(), array('dashboard', 'profile')))
+if(in_array($widget->context, array('dashboard', 'profile')))
 {
-	echo 	elgg_echo('event_manager:widgets:events:showevents').':'.
-			elgg_view('input/pulldown', array(	'internalname' => 'params[type_to_show]', 
-												'options' => array(	'all' => elgg_echo('all'),
-	 																'owning' => elgg_echo('event_manager:widgets:events:showevents:icreated'), 
-	 																'attending' => elgg_echo('event_manager:widgets:events:showevents:attendingto'))));
+	echo elgg_echo('event_manager:widgets:events:showevents') . ': ';
+	echo elgg_view('input/pulldown', array(	'internalname' => 'params[type_to_show]', 
+											'value' => $type_to_show, 
+											'options_values' => array(	'all' => elgg_echo('all'),
+			 															'owning' => elgg_echo('event_manager:widgets:events:showevents:icreated'), 
+			 															'attending' => elgg_echo('event_manager:widgets:events:showevents:attendingto'))));
 }
 ?>
 </p>
--- views/default/widgets/events/view.php
+++ views/default/widgets/events/view.php
@@ -8,27 +8,31 @@
 	set_context('widget');
 
 	$event_options = array();
-	$page_owner = page_owner_entity();
-	if(($current_context == 'groups') && $page_owner && ($page_owner instanceof ElggGroup))
-	{		
-		$event_options["container_guid"] = $page_owner->getGUID();
-	}
-	elseif(in_array($current_context, array('profile', 'dashboard')))
-	{
-		if($vars['entity']->type_to_show == 'i created')
-		{
-			$event_options["owning"] = true;
-		}
-		elseif($vars['entity']->type_to_show == 'i\'m attending to')
-		{
-			$event_options["meattending"] = true;
-		}
+	
+	switch($owner->getType()){
+		case "group":
+			$event_options["container_guid"] = $owner->getGUID();
+			break;
+		case "user":
+			switch($widget->type_to_show){
+				case "owning":
+					$event_options["owning"] = true;
+					break;
+				case "attending":
+					$event_options["meattending"] = true;
+					break;
+			}
+			break;
 	}
 	
-	$event_options["limit"] = $vars['entity']->num_display;
+	$num_display = (int) $widget->num_display;
+	if($num_display < 1){
+		$num_display = 5;
+	}
+	$event_options["limit"] = $num_display;
 	
 	$events = event_manager_search_events($event_options);
-	$content = elgg_view_entity_list($events['entities'], 0, 0, 5, false);	
+	$content = elgg_view_entity_list($events['entities'], $events["count"], 0, $num_display, false, false, false);	
 	
 	set_context($current_context);
 
