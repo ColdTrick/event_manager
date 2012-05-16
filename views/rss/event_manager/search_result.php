f3021af7dfc534d1953e233ff5438f0e577a501b
jdalsem <jdalsem@coldtrick.com>
2012-05-16 09:20:02 +0200

    fixed: deprecated notice

--- views/rss/event_manager/search_result.php
+++ views/rss/event_manager/search_result.php
@@ -1,3 +1,3 @@
 <?php 
 
-	echo elgg_view_entity_list($vars["entities"], $vars["count"], $vars["offset"], 10, false);
+	echo elgg_view_entity_list($vars["entities"], array("count" => $vars["count"], "offset" => $vars["offset"], "limit" => 10, "full_view" => false));
