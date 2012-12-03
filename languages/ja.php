90835b561b913c8fc1261c8d730756d473cb1ca4
yumokin2 <yumokin2@yumoto-no-MacBook-Air.local>
2012-12-04 04:53:24 +0900

    japanese languages
    I created japanese languages.

---
+++ languages/ja.php
@@ -0,0 +1,307 @@
+<?php 
+
+	$japanese = array(
+		'event_manager' => 'イベントマネージャー',
+		'groups:enableevents' => 'Enable group events',
+		'event_manager:group' => 'Group events',
+		'event_manager:group:more' => 'More events',
+	
+		'item:object:event' => 'イベント',
+		'item:object:eventslot' => 'Eventslots',
+		'item:object:eventday' => 'イベント日',
+		'item:object:eventregistration' => 'イベント登録',
+		'item:object:eventregistrationquestion' => 'Event Registration Questions',
+		'item:object:eventquestions' => 'Event Questions',
+	
+		'event_manager:menu:title' => 'イベント',
+		'event_manager:menu:events' => '全てのイベント',
+		'event_manager:menu:group_events' => 'グループのイベント',
+		'event_manager:menu:new_event' => '新しいイベント',
+	
+		'event_manager:sidebar:title' => 'Now showing',
+		
+		'river:comment:object:event' => '%s commented on %s',
+		
+		'event_manager:list:title' => 'イベント',
+		'event_manager:list:searchevents' => 'イベントを検索する',
+		'event_manager:list:advancedsearch' => '高機能な検索',
+		'event_manager:list:simplesearch' => 'シンプルな検索',
+		'event_manager:list:noresults' => 'イベントが見つかりません',
+		'event_manager:list:showmorevents' => 'もっと多くのイベントを表示する',
+		'event_manager:list:includepastevents' => '過去のイベントも含む',
+		'event_manager:list:meattending' => 'Show events i\'m  attending to',
+		'event_manager:list:owning' => 'Show events i created',
+		'event_manager:list:friendsattending' => 'Show イベント my friends are attending to',
+	
+		'event_manager:list:navigation:list' => 'リストでイベント確認',
+		'event_manager:list:navigation:all' => '全てのイベント',
+		'event_manager:list:navigation:other' => '他のイベント',
+		'event_manager:list:navigation:owned' => '自分のイベント',
+		'event_manager:list:navigation:your' => 'あなたのイベント',
+		'event_manager:list:navigation:friends' => '友達のイベント',
+		'event_manager:list:navigation:attending' => '参加しているイベント',
+		'event_manager:list:navigation:onthemap' => '地図でイベント確認',
+		'event_manager:list:navigation:refreshing' => 'refreshing イベント',
+		'event_manager:list:count' => ' イベント found',
+		'event_manager:full' => 'full',
+		'event_manager:personwaitinglist' => 'person on the waiting list',
+		'event_manager:peoplewaitinglist' => 'people on the waiting list',
+	
+		'event_manager:registration:list:navigation:waiting' => 'Waiting list',
+		'event_manager:registration:list:navigation:attending' => 'Attending',
+	
+		'event_manager:registration:view:savetopdf' => 'Save as pdf',
+		
+		'event_manager:edit:title' => 'イベントを作る、編集する',
+		'event_manager:edit:upload:title' => 'Add files to your イベント',
+		'event_manager:edit:form:file' => 'Choose file',
+		'event_manager:edit:form:title' => 'タイトル',
+		'event_manager:edit:form:description' => '説明',
+		'event_manager:edit:form:files' => 'ファイル',
+		'event_manager:edit:form:venue' => '開催地',
+		'event_manager:edit:form:type' => 'Type',
+		'event_manager:edit:form:location' => '場所',
+		'event_manager:event:edit:maps_address' => '住所',
+		'event_manager:edit:form:region' => 'Region',
+		'event_manager:edit:form:options' => 'オプション',
+		'event_manager:edit:form:rsvp_options' => '参加オプション',
+		'event_manager:edit:form:shortdescription' => '簡単な説明',
+		'event_manager:edit:form:organizer' => '主催者',
+		'event_manager:edit:form:with_program' => 'Specify daily program?',
+		'event_manager:edit:form:delete_current_icon' => 'Delete current icon?',
+		'event_manager:edit:form:comments_on' => 'Comments on?',
+		'event_manager:edit:form:registration_ended' => 'Disable registration to this イベント',
+		'event_manager:edit:form:registration_needed' => 'Registration needed to attend?',
+		'event_manager:edit:form:show_attendees' => 'Display attendees?',
+		'event_manager:edit:form:notify_onsignup' => 'Notify me when someone signs up',
+		'event_manager:edit:form:start_day' => '開始日',
+		'event_manager:edit:form:start_day:from' => 'Date from',
+		'event_manager:edit:form:start_day:to' => 'Date to',
+		'event_manager:edit:form:endregistration_day' => 'End date of registration',
+		'event_manager:edit:form:multiple_days' => 'Multiple days?',
+		'event_manager:edit:form:start_time' => '開始時間',
+		'event_manager:edit:form:end_time' => '終了時間',
+		'event_manager:edit:form:access' => 'Access',
+		'event_manager:edit:form:max_attendees' => 'Maximum attendees',
+		'event_manager:edit:form:waiting_list' => 'Enable waiting list?',
+		'event_manager:edit:form:register_nologin' => 'Allow not-loggedin users to register?',
+		'event_manager:edit:form:spots_left' => 'Spots left',
+		'event_manager:edit:form:spots_left:full' => 'Activity is full',
+		'event_manager:edit:form:spots_left:waiting_list' => ' attendee(s) on the waiting list',
+		'event_manager:edit:form:currenticon' => '現在のアイコン',
+		'event_manager:edit:form:icon' => 'イベントアイコン (leave blank to leave unchanged)',
+	
+		'event_manager:form:program:day' => 'Add イベント day',
+		'event_manager:program:day:add' => 'Add day',
+		'event_manager:form:program:slot' => 'Add activity slot',
+		'event_manager:program:slot:add' => 'Add slot',
+		
+		'event_manager:editregistration:title' => 'Edit イベント registration form',
+		'event_manager:editregistration:addfield' => 'Add field',
+		'event_manager:editregistration:addfield:title' => 'Add registrationform field',
+		'event_manager:editregistration:fieldtype' => 'Field type',
+		'event_manager:editregistration:fieldoptions' => 'Field options',
+		'event_manager:editregistration:commasepetared' => 'comma separated',
+		'event_manager:editregistration:question' => '質問',
+	
+		'event_manager:registration:message:registrationnotneeded' => 'Registration to this イベント is not needed.',	
+		'event_manager:registration:register:title' => 'Register to イベント',	
+		'event_manager:registration:registrationto' => 'Registration to ',	
+		'event_manager:registration:edit:title' => 'Edit your registration',	
+		'event_manager:registration:edityourregistration' => 'Edit your registration',	
+		'event_manager:registration:viewyourregistration' => 'View your registration',	
+		'event_manager:registration:yourregistration' => 'Your registration',	
+		'event_manager:registration:programparticipation' => 'Program participation',
+		'event_manager:registration:editprogramparticipation' => 'Edit your program participation',
+	
+		'event_manager:registration:program:success' => 'Succesfully (un)registered to the selected slots',
+		'event_manager:registration:program:fail' => 'Failed to (un)register to the selected slots',
+		
+		'event_manager:event:registration:notification:owner:subject' => 'イベント registration',	
+		'event_manager:event:registration:notification:user:subject' => 'イベント registration',
+	
+		'event_manager:event:registration:notification:owner:text:event_attending' => "Hello %s,
+
+%s signed up as 'attending' to your '%s' イベント.",
+		
+		'event_manager:event:registration:notification:user:text:event_attending' => "Hello %s,
+
+You succesfully signed up as 'attending' to the '%s' イベント.",
+	
+		'event_manager:event:registration:notification:owner:text:event_waitinglist' => "Hello %s,
+
+%s is on the waiting list for your '%s' イベント.",
+		
+		'event_manager:event:registration:notification:user:text:event_waitinglist' => "Hello %s,
+
+You are now on the waiting list for the '%s' イベント.",	
+	
+		'event_manager:event:registration:notification:owner:text:event_exhibiting' => "Hello %s,
+
+%s has signed up as 'exhibiting' to your '%s' イベント.",
+		
+		'event_manager:event:registration:notification:user:text:event_exhibiting' => "Hello %s,
+
+You succesfully signed up as 'exhibiting' to the '%s' イベント.",	
+	
+		'event_manager:event:registration:notification:owner:text:event_organizing' => "Hello %s,
+
+%s has signed up as 'organizing' to your '%s' event.",
+		
+		'event_manager:event:registration:notification:user:text:event_organizing' => "Hello %s,
+
+You succesfully signed up as 'organizing' to the '%s' イベント.",	
+	
+		'event_manager:event:registration:notification:owner:text:event_presenting' => "Hello %s,
+
+%s has signed up as 'presenter' to your '%s' イベント.",
+		
+		'event_manager:event:registration:notification:user:text:event_presenting' => "Hello %s,
+
+You succesfully signed up as 'presenter' to the '%s' イベント.",	
+	
+		'event_manager:event:registration:notification:owner:text:event_interested' => "Hello %s,
+
+%s has signed up as 'interested' to your '%s' イベント.",
+		
+		'event_manager:event:registration:notification:user:text:event_interested' => "Hello %s,
+
+You succesfully signed up as 'interested' to the '%s' イベント.",	
+	
+		'event_manager:event:registration:notification:owner:text:event_undo' => "Hello %s,
+
+%s has signed off from your '%s' イベント.",
+		
+		'event_manager:event:registration:notification:user:text:event_undo' => "Hello %s,
+
+You signed off succesfully from the '%s' イベント.",		
+	
+		'event_manager:event:registration:notification:user:text:event_spotfree' => "Hello %s,
+
+Someone signed off from the '%s' イベント. You are moved from the waiting list to attending.",	
+	
+		'event_manager:event:registration:notification:program:linktext' => 'To view the program click on this link',
+	
+		'event_manager:event:rsvp' => '参加確認',
+		'event_manager:event:editevent' => 'イベントを編集する',
+		'event_manager:event:deleteevent' => 'イベントを削除する',
+		'event_manager:event:uploadfiles' => 'ファイルをアップロードする',
+		'event_manager:event:attendees' => '参加費',
+		'event_manager:event:progam' => '内容',
+		'event_manager:event:editprogram' => '内容を編集する',
+		'event_manager:event:editquestions' => 'Edit registration questions',
+		'event_manager:event:viewwaitinglist' => 'View waiting list',
+		'event_manager:event:waitinglist:empty' => 'Waiting list is empty',
+		'event_manager:event:viewregistrations' => 'View registrations',
+		'event_manager:event:viewregistration' => 'View registration',
+		'event_manager:event:noregistrations' => 'No registrations yet',
+		'event_manager:event:exportattendees' => 'Export attendees',
+		'event_manager:event:currentavatar' => 'Current event icon',
+		'event_manager:event:editicon' => 'Edit icon',
+		'event_manager:event:preview' => 'Preview icon',
+		'event_manager:event:createicon' => 'Create icon',
+	
+		'event_manager:registrationform:editquestion:required' => 'Required?',
+		'event_manager:registrationform:fieldorder:error' => 'Error changing field order',
+	
+		'event_manager:event:file:notfound:title' => 'File not found',
+		'event_manager:event:file:notfound:text' => 'The file you are looking for couldn\'t be found',
+	
+		'event_manager:event:register:register_link' => 'Register to this event',
+		'event_manager:event:register:no_registrationform' => 'The organizer of this event hasn\'t created a registration form yet.<br />Please try again later.',
+	
+		'event_manager:event:view:event' => 'イベント',
+		'event_manager:event:view:date' => '日付',
+		'event_manager:event:view:createdby' => 'によって作成',
+	
+		//Error
+		'event_manager:login_needed' => 'You need to be logged in.',
+		'event_manager:event_not_found' => 'イベントが見つかりません',
+		'event_manager:relationship_type_not_found' => 'Relationship type not found.',
+		'event_manager:error:max_attendees:not_numeric' => 'Value for \'Max Attendees\' must be a numeric value.',
+	
+		// relationships
+		'event_manager:event:relationship:join' => 'イベントに参加する',
+		'event_manager:event:relationship:leave' => 'イベントを去る',
+		'event_manager:event:relationship:event_attending' => '参加している',
+		'event_manager:event:relationship:event_waitinglist' => 'Waiting list',
+		'event_manager:event:relationship:event_interested' => '興味がある',
+		'event_manager:event:relationship:event_presenting' => '参加している',		
+		'event_manager:event:relationship:event_exhibiting' => '公開している',
+		'event_manager:event:relationship:event_organizing' => '主催している',
+		'event_manager:event:relationship:undo' => '元に戻す',
+		'event_manager:event:relationship:kick' => 'Kick from event',
+	
+		'event_manager:event:rsvp:registration_ended' => 'Registration to this event has been ended.',
+		'event_manager:event:rsvp:event_full' => 'This event reached maximum attendees.',
+		'event_manager:event:rsvp:waiting_list' => 'Waiting list',
+		'event_manager:event:rsvp:nospotsleft' => 'This event has no spots left for it\'s activities.',
+		'event_manager:event:rsvp:waiting_list:message' => 'The event you want to attend to is full. Fill in the form below to get on the waiting list.',
+	
+		'event_manager:event:relationship:message:event_attending' => 'You are now attending to this event',
+		'event_manager:event:relationship:message:event_waitinglist' => 'You are now on the waiting list for this event',
+		'event_manager:event:relationship:message:event_interested' => 'あなたはこのイベントに興味がある',
+		'event_manager:event:relationship:message:event_presenting' => 'あなたはこのイベントに参加している',
+		'event_manager:event:relationship:message:event_exhibiting' => 'あなたはこのイベントを公開している',
+		'event_manager:event:relationship:message:event_organizing' => 'あなたはこのイベントを組織している',
+		'event_manager:event:relationship:message:event_undo' => 'You left this event',
+		'event_manager:event:relationship:message:error' => 'Error joining/leaving event',
+		'event_manager:event:relationship:message:unavailable_relation' => 'The RSVP type you selected is not available',
+	
+		// widgets
+		'event_manager:widgets:events:title' => 'イベントマネージャー',
+		'event_manager:widgets:events:description' => 'Show upcoming events',
+	
+		'event_manager:widgets:events:numbertodisplay' => 'Number of events to display',
+		'event_manager:widgets:events:showevents' => 'Show events',
+		'event_manager:widgets:events:showevents:icreated' => 'i created',
+		'event_manager:widgets:events:showevents:attendingto' => 'i\'m attending to',
+	
+		// actions
+		'event_manager:action:event:edit:ok' => 'イベント saved',
+		'event_manager:action:event:edit:error_fields' => 'Fill in the required fields',
+		'event_manager:action:registration:edit:error_fields_with_program' => 'Fill in the required fields and select at least 1 activity slot to attend to',
+		'event_manager:action:registration:edit:error_fields_program_only' => 'Select at least 1 activity slot to attend to',
+		'event_manager:action:event:delete:ok' => 'イベント deleted',
+	
+		// settings
+		'event_manager:settings:google_maps' => 'Google Maps settings',
+		'event_manager:settings:google_api_key' => 'Enter your Google API key',
+		'event_manager:settings:google_maps:enterdefaultlocation' => 'Enter a default location google maps needs to center on',
+		'event_manager:settings:google_maps:enterdefaultzoom' => 'Enter a default google maps zoom level (0 = zoomed in, 19 = zoomed out)',
+		'event_manager:settings:google_api_key:clickhere' => 'Go to <a target="_blank" href="https://developers.google.com/maps/documentation/javascript/tutorial#api_key">https://developers.google.com/maps/documentation/javascript/tutorial#api_key</a> to get your "Google API key"',
+		'event_manager:settings:other' => 'Other',
+		'event_manager:settings:region_list' => 'Enter (comma separated) event regions',
+		'event_manager:settings:type_list' => 'Enter (comma separated) event types',
+		'event_manager:settings:migration' => 'Migrate %d events from "イベント calender" plugin',	
+		'event_manager:settings:migration:success' => 'Succesfully migrated %d events',	
+		'event_manager:settings:migration:noeventstomigrate' => 'No events found to migrate',
+		'event_manager:settings:notification_sender' => 'RSVP notification sender (email)',
+	
+		'event_manager:settings:migration:site:whocancreate' => 'Who can create site events',
+		'event_manager:settings:migration:site:whocancreate:admin_only' => 'admin only',
+		'event_manager:settings:migration:site:whocancreate:everyone' => 'everyone',
+		
+		'event_manager:settings:migration:group:whocancreate' => 'Who can create group events',	
+		'event_manager:settings:migration:group:whocancreate:group_admin' => 'group owners and admins only',	
+		'event_manager:settings:migration:group:whocancreate:members' => 'any group member',	
+		'event_manager:settings:migration:group:whocancreate:no_one' => 'no one',	
+	
+	
+		// river
+		'event_manager:river:event_relationship:create:event_attending' => '%s is attending %s',
+		'event_manager:river:event_relationship:create:event_interested' => '%s is interested in %s',
+		'event_manager:river:event_relationship:create:event_presenting' => '%s is presenting at %s',
+		'event_manager:river:event_relationship:create:event_exhibiting' => '%s is exhibiting at %s',
+		'event_manager:river:event_relationship:create:event_organizing' => '%s is organizing %s',
+	
+		'calculate_route' => 'Calculate route',
+	
+		'from' => 'From',
+		'to' => 'To',
+		'approve' => 'Approve',	
+		'disapprove' => 'Disapprove',	
+		'requiredfields' => '必須項目',
+	);
+	
+	add_translation('ja', $japanese);
