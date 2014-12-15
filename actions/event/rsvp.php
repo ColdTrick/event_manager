0aa969d7151d1a3e25cca182fa06ee985ea6f8cf
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2014-12-15 14:01:53 +0200

    Flattens structure of the RSVP action

--- actions/event/rsvp.php
+++ actions/event/rsvp.php
@@ -8,56 +8,58 @@ $forward_url = get_input("forward_url", REFERER);
 $notice = true;
 $rsvp = null;
 
-if (!empty($guid) && !empty($rel)) {
-	$event = get_entity($guid);
-	$user = get_entity($user_guid);
-	
-	if (!empty($event) && elgg_instanceof($event, "object", Event::SUBTYPE) && !empty($user)) {
-		if ($rel == EVENT_MANAGER_RELATION_ATTENDING) {
-			if ($event->hasEventSpotsLeft() && $event->hasSlotSpotsLeft()) {
+if (empty($guid) || empty($rel)) {
+	register_error(elgg_echo("IOException:FailedToLoadGUID", array("Event", $guid)));
+	forward(REFERER);
+}
+
+$event = get_entity($guid);
+$user = get_entity($user_guid);
+
+if (!$event instanceof Event) {
+	register_error(elgg_echo("InvalidClassException:NotValidElggStar", array($guid, "Event")));
+	forward(REFERER);
+}
+
+if ($rel == EVENT_MANAGER_RELATION_ATTENDING) {
+	if ($event->hasEventSpotsLeft() && $event->hasSlotSpotsLeft()) {
+		if ($event->registration_needed && $event->hasRegistrationForm()) {
+			$forward_url = '/events/event/register/' . $guid . '/' . $rel;
+			$notice = false;
+		} else {
+			$rsvp = $event->rsvp($rel, $user_guid);
+		}
+	} else {
+		if ($event->waiting_list_enabled) {
+			$rel = EVENT_MANAGER_RELATION_ATTENDING_WAITINGLIST;
+			if ($event->openForRegistration()) {
 				if ($event->registration_needed && $event->hasRegistrationForm()) {
-					$forward_url = '/events/event/register/' . $guid . '/' . $rel;
+					$forward_url = '/events/event/waitinglist/' . $guid;
 					$notice = false;
 				} else {
 					$rsvp = $event->rsvp($rel, $user_guid);
 				}
 			} else {
-				if ($event->waiting_list_enabled) {
-					$rel = EVENT_MANAGER_RELATION_ATTENDING_WAITINGLIST;
-					if ($event->openForRegistration()) {
-						if ($event->registration_needed && $event->hasRegistrationForm()) {
-							$forward_url = '/events/event/waitinglist/' . $guid;
-							$notice = false;
-						} else {
-							$rsvp = $event->rsvp($rel, $user_guid);
-						}
-					} else {
-						register_error(elgg_echo('event_manager:event:rsvp:registration_ended'));
-					}
-				} else {
-					register_error(elgg_echo('event_manager:event:rsvp:nospotsleft'));
-				}
+				register_error(elgg_echo('event_manager:event:rsvp:registration_ended'));
 			}
 		} else {
-			if ($event->$rel || ($rel == EVENT_MANAGER_RELATION_UNDO && ($event->canEdit() || $user->canEdit()))) {
-				$rsvp = $event->rsvp($rel, $user_guid);
-			} else {
-				register_error(elgg_echo('event_manager:event:relationship:message:unavailable_relation'));
-			}
+			register_error(elgg_echo('event_manager:event:rsvp:nospotsleft'));
 		}
-		
-		if ($notice) {
-			if ($rsvp) {
-				system_message(elgg_echo('event_manager:event:relationship:message:' . $rel));
-			} else {
-				register_error(elgg_echo('event_manager:event:relationship:message:error'));
-			}
-		}
-	} else {
-		register_error(elgg_echo("InvalidClassException:NotValidElggStar", array($guid, "Event")));
 	}
 } else {
-	register_error(elgg_echo("IOException:FailedToLoadGUID", array("Event", $guid)));
+	if ($event->$rel || ($rel == EVENT_MANAGER_RELATION_UNDO && ($event->canEdit() || $user->canEdit()))) {
+		$rsvp = $event->rsvp($rel, $user_guid);
+	} else {
+		register_error(elgg_echo('event_manager:event:relationship:message:unavailable_relation'));
+	}
+}
+
+if ($notice) {
+	if ($rsvp) {
+		system_message(elgg_echo('event_manager:event:relationship:message:' . $rel));
+	} else {
+		register_error(elgg_echo('event_manager:event:relationship:message:error'));
+	}
 }
 
 forward($forward_url);
