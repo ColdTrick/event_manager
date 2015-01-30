caa81b7c76f0ac021465e79285b28bb6ba4389ee
Jeroen Dalsem <jdalsem@coldtrick.com>
2015-01-30 13:26:29 +0100

    moved page handler to own lib file

---
+++ lib/page_handlers.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Page handlers for Event Manager
+ */
+
+/**
+ * Page handler
+ *
+ * @param array $page page elements
+ *
+ * @return boolean
+ */
+function event_manager_page_handler($page) {
+	elgg_load_js("event_manager.maps.base");
+	elgg_load_js("event_manager.maps.helper");
+
+	elgg_push_breadcrumb(elgg_echo("event_manager:menu:events"), "/events");
+
+	$include = "/pages/event/list.php";
+	if (!empty($page)) {
+		switch ($page[0]) {
+			case "proc":
+				if (file_exists(dirname(dirname(__FILE__)) . "/procedures/" . $page[1] . "/" . $page[2] . ".php")) {
+					$include = "/procedures/" . $page[1] . "/" . $page[2] . ".php";
+				} else {
+					echo json_encode(array("valid" => 0));
+					exit();
+				}
+				break;
+			case "unsubscribe":
+				if (isset($page[1])) {
+					if ($page[1] == "confirm") {
+						if (isset($page[2])) {
+							set_input("guid", $page[2]);
+						}
+
+						if (isset($page[3])) {
+							set_input("code", $page[3]);
+						}
+
+						$include = "/pages/event/unsubscribe_confirm.php";
+					} else {
+						set_input("guid", $page[1]);
+
+						$include = "/pages/event/unsubscribe.php";
+					}
+				}
+				break;
+			case "registration":
+				if (isset($page[1])) {
+					switch ($page[1]) {
+						case "confirm":
+							if (isset($page[2])) {
+								set_input("event_guid", $page[2]);
+							}
+
+							$include = "/pages/registration/confirm.php";
+							break(2);
+						case "completed":
+							if (isset($page[2])) {
+								set_input("event_guid", $page[2]);
+							}
+
+							if (isset($page[3])) {
+								set_input("object_guid", $page[3]);
+							}
+
+							$include = "/pages/registration/completed.php";
+							break(2);
+					}
+				}
+			case "event":
+				switch ($page[1]) {
+					case "register":
+						if (!empty($page[3])) {
+							set_input("relation", $page[3]);
+						}
+						break;
+					case "file":
+						if (!empty($page[3])) {
+							set_input("file", $page[3]);
+						}
+						break;
+					case "list":
+						set_input("owner_guid", $page[2]);
+						break;
+					case "new":
+						$page[1] = "edit";
+						set_input("owner_guid", $page[2]);
+				}
+			default:
+				if (!empty($page[2]) && ($page[1] !== "new")) {
+					set_input("guid", $page[2]);
+				}
+
+				if (file_exists(dirname(dirname(__FILE__)) . "/pages/" . $page[0] . "/" . $page[1] . ".php")) {
+					$include = "/pages/" . $page[0] . "/" . $page[1] . ".php";
+				} else {
+					forward("/events");
+				}
+				break;
+		}
+	}
+
+	include(dirname(dirname(__FILE__)) . $include);
+
+	return true;
+}
