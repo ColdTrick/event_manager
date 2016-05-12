e496672c351afe24a98053f7aa04701fe13a57b4
Jeroen Dalsem <jdalsem@coldtrick.com>
2016-05-12 11:24:04 +0200

    changed: use icons instead of labels for contact details

--- views/default/event_manager/event/sidebar/contact_details.php
+++ views/default/event_manager/event/sidebar/contact_details.php
@@ -13,15 +13,17 @@ $twitter_hash = $event->twitter_hash;
 $organizer = $event->organizer;
 
 if ($organizer) {
-	
-	$contact_information .= '<label>' . elgg_echo('event_manager:edit:form:organizer') . '</label>';
-	$contact_information .= '<div>' . $organizer . '</div>';
+	$contact_information .= '<tr>';
+	$contact_information .= '<td class="prs">' . elgg_view_icon('user', ['title' => elgg_echo('event_manager:edit:form:organizer')]) . '</td>';
+	$contact_information .= '<td>' . $organizer . '</td>';
+	$contact_information .= '</tr>';
 }
 
 if ($contact_details) {
-
-	$contact_information .= '<label>' . elgg_echo('event_manager:edit:form:contact_details') . '</label>';
-	$contact_information .= '<div>' . elgg_view('output/text', ['value' => $contact_details]) . '</div>';
+	$contact_information .= '<tr>';
+	$contact_information .= '<td class="prs">' . elgg_view_icon('info-circle', ['title' => elgg_echo('event_manager:edit:form:contact_details')]) . '</td>';
+	$contact_information .= '<td>' . elgg_view('output/text', ['value' => $contact_details]) . '</td>';
+	$contact_information .= '</tr>';
 }
 
 if ($website) {
@@ -29,18 +31,23 @@ if ($website) {
 		$website = "http://$website";
 	}
 
-	$contact_information .= '<label>' . elgg_echo('event_manager:edit:form:website') . '</label>';
-	$contact_information .= '<div>' . elgg_view('output/url', ['href' => $website]) . '</div>';
+	$contact_information .= '<tr>';
+	$contact_information .= '<td class="prs">' . elgg_view_icon('globe', ['title' => elgg_echo('event_manager:edit:form:website')]) . '</td>';
+	$contact_information .= '<td>' . elgg_view('output/url', ['href' => $website]) . '</td>';
+	$contact_information .= '</tr>';
 }
 
 if ($twitter_hash) {
-	$contact_information .= '<label>' . elgg_echo('event_manager:edit:form:twitter_hash') . '</label>';
-	$contact_information .= '<div>' . elgg_view('output/url', [
+	$contact_information .= '<tr>';
+	$contact_information .= '<td class="prs">' . elgg_view_icon('twitter', ['title' => elgg_echo('event_manager:edit:form:twitter_hash')]) . '</td>';
+	$contact_information .= '<td>' . elgg_view('output/url', [
 		'value' => 'http://twitter.com/search?q=' . urlencode($twitter_hash),
 		'text' => elgg_view('output/text', ['value' => $twitter_hash]),
-	]) . '</div>';
+	]) . '</td>';
+	$contact_information .= '</tr>';
 }
 
 if (!empty($contact_information)) {
+	$contact_information = elgg_format_element('table', [], $contact_information);
 	echo elgg_view_module('aside', 'Contact details', $contact_information);
 }
