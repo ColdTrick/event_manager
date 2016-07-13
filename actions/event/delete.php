097d6e2e893d75f6f241e814ad467bf8164879ef
Jeroen Dalsem <jdalsem@coldtrick.com>
2016-07-13 15:22:26 +0200

    fixed: correct forward url after delete of group event

--- actions/event/delete.php
+++ actions/event/delete.php
@@ -4,9 +4,15 @@ $guid = (int) get_input('guid');
 elgg_entity_gatekeeper($guid, 'object', Event::SUBTYPE);
 $entity = get_entity($guid);
 
+$forward = 'events';
+$container = $entity->getContainerEntity();
+if ($container instanceof \ElggGroup) {
+	$forward = 'events/event/list/' . $container->getGUID();
+}
+
 if ($entity->delete()) {
 	system_message(elgg_echo('entity:delete:success'));
-	forward('events');
+	forward($forward);
 } else {
 	register_error(elgg_echo('entity:delete:fail', [$entity->title]));
 }
