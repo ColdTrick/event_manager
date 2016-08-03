efa342b4aff6420754867f211ebeacaa9a58bc98
jdalsem <jdalsem@coldtrick.com>
2016-08-03 11:22:05 +0200

    added: parts of event edit form now show collapsed if no values set

--- views/default/forms/event_manager/event/tabs/contact.php
+++ views/default/forms/event_manager/event/tabs/contact.php
@@ -1,19 +1,50 @@
 <?php
 
-echo elgg_view_input('text', [
+$organizer = elgg_extract('organizer', $vars);
+$contact_details = elgg_extract('contact_details', $vars);
+$website = elgg_extract('website', $vars);
+
+$collapsed = true;
+if (!empty($organizer) || !empty($contact_details) || !empty($website)) {
+	$collapsed = false;
+}
+
+$output = elgg_view_input('text', [
 	'label' => elgg_echo('event_manager:edit:form:organizer'),
 	'name' => 'organizer',
-	'value' => $vars['organizer'],
+	'value' => $organizer,
 ]);
 
-echo elgg_view_input('text', [
+$output .= elgg_view_input('text', [
 	'label' => elgg_echo('event_manager:edit:form:contact_details'),
 	'name' => 'contact_details',
-	'value' => $vars['contact_details'],
+	'value' => $contact_details,
 ]);
 
-echo elgg_view_input('url', [
+$output .= elgg_view_input('url', [
 	'label' => elgg_echo('event_manager:edit:form:website'),
 	'name' => 'website',
-	'value' => $vars['website'],
+	'value' => $website,
 ]);
+
+if (!$collapsed) {
+	echo $output;
+	return;
+}
+
+$toggle_button = elgg_view('input/button', [
+	'class' => 'elgg-button-action',
+	'value' => elgg_echo('event_manager:edit:form:tabs:contact:toggle'),
+	'rel' => 'toggle',
+	'data-toggle-selector' => '.event-manager-edit-contact-toggle',
+]);
+
+echo elgg_format_element('div', [
+	'class' => 'event-manager-edit-contact-toggle center',
+	'data-toggle-slide' => 0,
+], $toggle_button);
+
+echo elgg_format_element('div', [
+	'class' => 'hidden event-manager-edit-contact-toggle',
+	'data-toggle-slide' => 0,
+], $output);
--- views/default/forms/event_manager/event/tabs/location.php
+++ views/default/forms/event_manager/event/tabs/location.php
@@ -3,16 +3,27 @@
  * Form fields for event location data
  */
 
-echo elgg_view_input('text', [
+$venue = elgg_extract('venue', $vars);
+$location = elgg_extract('location', $vars);
+$region = elgg_extract('region', $vars);
+
+$region_options = event_manager_event_region_options();
+
+$collapsed = true;
+if (!empty($venue) || !empty($location) || (!empty($region) && $region_options)) {
+	$collapsed = false;
+}
+
+$output = elgg_view_input('text', [
 	'label' => elgg_echo('event_manager:edit:form:venue'),
 	'name' => 'venue',
-	'value' => $vars['venue'],
+	'value' => $venue,
 ]);
 
-echo elgg_view_input('text', [
+$output .= elgg_view_input('text', [
 	'label' => elgg_echo('event_manager:edit:form:location'),
 	'name' => 'location',
-	'value' => $vars['location'],
+	'value' => $location,
 	'class' => 'elgg-lightbox',
 	'data-colorbox-opts' => json_encode([
 		'inline' => true,
@@ -21,18 +32,39 @@ echo elgg_view_input('text', [
 	'readonly' => true,
 ]);
 
-echo '<div class="hidden">';
-echo elgg_format_element('div', [
+$output .= '<div class="hidden">';
+$output .= elgg_format_element('div', [
 	'id' => 'event-manager-edit-maps-search-container',
 ], elgg_view('event_manager/event/maps/select_location'));
-echo '</div>';
+$output .= '</div>';
 
-$region_options = event_manager_event_region_options();
 if ($region_options) {
-	echo elgg_view_input('select', [
+	$output .= elgg_view_input('select', [
 		'label' => elgg_echo('event_manager:edit:form:region'),
 		'name' => 'region',
-		'value' => $vars['region'],
+		'value' => $region,
 		'options' => $region_options,
 	]);
 }
+
+if (!$collapsed) {
+	echo $output;
+	return;
+}
+
+$toggle_button = elgg_view('input/button', [
+	'class' => 'elgg-button-action',
+	'value' => elgg_echo('event_manager:edit:form:tabs:location:toggle'),
+	'rel' => 'toggle',
+	'data-toggle-selector' => '.event-manager-edit-location-toggle',
+]);
+
+echo elgg_format_element('div', [
+	'class' => 'event-manager-edit-location-toggle center',
+	'data-toggle-slide' => 0,
+], $toggle_button);
+
+echo elgg_format_element('div', [
+	'class' => 'hidden event-manager-edit-location-toggle',
+	'data-toggle-slide' => 0,
+], $output);
