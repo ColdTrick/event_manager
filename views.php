96b3a0a17cfa6c9e56b45b20f6f73240e4a67ee5
Jeroen Dalsem <jdalsem@coldtrick.com>
2016-08-23 11:25:12 +0200

    fixed: correctly link views from composer dependencies

--- views.php
+++ views.php
@@ -2,8 +2,8 @@
 return [
     // viewtype
     'default' => [
-       'js/fullcalendar.js' => __DIR__ . '/vendor/bower-asset/fullcalendar/dist/fullcalendar.min.js',
-       'js/moment.js' => __DIR__ . '/vendor/bower-asset/moment/min/moment-with-locales.min.js',
-       'css/event_manager/fullcalendar.css' => __DIR__ . '/vendor/bower-asset/fullcalendar/dist/fullcalendar.min.css',
+       'js/fullcalendar.js' => 'vendor/bower-asset/fullcalendar/dist/fullcalendar.min.js',
+       'js/moment.js' => 'vendor/bower-asset/moment/min/moment-with-locales.min.js',
+       'css/event_manager/fullcalendar.css' => 'vendor/bower-asset/fullcalendar/dist/fullcalendar.min.css',
     ],
 ];
