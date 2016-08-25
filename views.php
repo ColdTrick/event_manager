1e570ceaf6c29f503118e2a6d755dc7636decbc4
Jeroen Dalsem <jdalsem@coldtrick.com>
2016-08-25 10:10:40 +0200

    fixed: autodetect composer installed dependencies or bundled

--- views.php
+++ views.php
@@ -1,9 +1,14 @@
 <?php
+
+$composer_path = '';
+if (is_dir(__DIR__ . '/vendor')) {
+	$composer_path = __DIR__ . '/';
+}
 return [
     // viewtype
     'default' => [
-       'js/fullcalendar.js' => 'vendor/bower-asset/fullcalendar/dist/fullcalendar.min.js',
-       'js/moment.js' => 'vendor/bower-asset/moment/min/moment-with-locales.min.js',
-       'css/event_manager/fullcalendar.css' => 'vendor/bower-asset/fullcalendar/dist/fullcalendar.min.css',
+       'js/fullcalendar.js' => $composer_path . 'vendor/bower-asset/fullcalendar/dist/fullcalendar.min.js',
+       'js/moment.js' => $composer_path . 'vendor/bower-asset/moment/min/moment-with-locales.min.js',
+       'css/event_manager/fullcalendar.css' => $composer_path . 'vendor/bower-asset/fullcalendar/dist/fullcalendar.min.css',
     ],
 ];
