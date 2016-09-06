d4c194a8e8feea1a58b3fa9bb3a22684e36fde87
Jeroen Dalsem <jdalsem@coldtrick.com>
2016-09-06 15:41:29 +0200

    fixed: addevent buttons in mail look better in various emailclients

--- views/default/event_manager/email/addevent.php
+++ views/default/event_manager/email/addevent.php
@@ -1,12 +1,15 @@
 <?php
 $event = elgg_extract('entity', $vars);
 
+// "outlook" or "google" or "appleical" or "outlookcom" or "yahoo"
+$services = (array) elgg_extract('services', $vars, ['appleical', 'google', 'outlook', 'outlookcom', 'yahoo']);
+if (empty($services)) {
+	return;
+}
+
 echo '<br /><br />';
 echo '<span class="event-manager-email-addevent-title">' . elgg_echo('event_manager:addevent:mail:title') . '</span>';
 echo '<br /><br />';
-
-// "outlook" or "google" or "appleical" or "outlookcom" or "yahoo"
-$services = (array) elgg_extract('services', $vars, ['appleical', 'google', 'outlook', 'outlookcom', 'yahoo']);
 
 $url_params = [
 	'client' => 'ak1qmrp10zvwxx2cimhv206',
@@ -20,12 +23,16 @@ $url_params = [
 	'location' => $event->location ?: $event->venue,
 ];
 
+echo '<table><tr>';
 foreach ($services as $service) {
 	$url_params['service'] = $service;
-	
-	echo elgg_view('output/url', [
+
+	$link = elgg_view('output/url', [
 		'href' => elgg_http_add_url_query_elements('https://addevent.com/dir/', $url_params),
 		'text' => elgg_echo('event_manager:addevent:mail:service:' . $service),
-		'class' => 'event-manager-email-addevent event-manager-email-addevent-' . $service,
 	]);
+	echo elgg_format_element('td', [
+		'class' => 'event-manager-email-addevent event-manager-email-addevent-' . $service,
+	], $link);
 }
+echo '</tr></table>';
