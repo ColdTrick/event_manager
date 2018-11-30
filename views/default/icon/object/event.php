af9adf660a9b5a9afe13ef754948592d44791f26
Jeroen Dalsem <jdalsem@coldtrick.com>
2018-11-30 09:23:36 +0100

    changed: event icon shows current date if event is underway

--- views/default/icon/object/event.php
+++ views/default/icon/object/event.php
@@ -9,6 +9,12 @@ if ($size !== 'date') {
 
 $event_start = $entity->getStartTimestamp();
 
+$event_end = $entity->getEndTimestamp();
+if (($event_start < time()) && ($event_end > time())) {
+	// show the running date
+	$event_start = time();
+}
+
 $icon = "<div class='event_manager_event_list_icon' title='" . event_manager_format_date($event_start) . "'>";
 $icon .= "<div class='event_manager_event_list_icon_month'>" . strtoupper(trim(elgg_echo('date:month:short:' . date("m", $event_start), ['']))) . "</div>";
 $icon .= "<div class='event_manager_event_list_icon_day'>" . date("d", $event_start) . "</div>";
