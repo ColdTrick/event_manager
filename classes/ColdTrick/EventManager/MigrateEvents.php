c447c49a7eec439230dda69d09339d879f78d516
Jerome Bakker <jeabakker@coldtrick.com>
2018-12-07 11:33:37 +0100

    fixed: entitiy tools integration
    fixes #122

--- classes/ColdTrick/EventManager/MigrateEvents.php
+++ classes/ColdTrick/EventManager/MigrateEvents.php
@@ -9,29 +9,40 @@ class MigrateEvents extends Migrate {
 	/**
 	 * Add events to the supported types for EntityTools
 	 *
-	 * @param string $hook         the name of the hook
-	 * @param string $type         the type of the hook
-	 * @param array  $return_value current return value
-	 * @param mixed  $params       supplied params
+	 * @param \Elgg\Hook $hook 'supported_types', 'entity_tools'
 	 *
 	 * @return array
 	 */
-	public static function supportedSubtypes($hook, $type, $return_value, $params) {
+	public static function supportedSubtypes(\Elgg\Hook $hook) {
 		
-		$return_value[\Event::SUBTYPE] = self::class;
+		$result = $hook->getValue();
 		
-		return $return_value;
+		$result[\Event::SUBTYPE] = static::class;
+		
+		return $result;
 	}
 	
 	/**
 	 * {@inheritDoc}
-	 * @see \ColdTrick\EntityTools\Migrate::setSupportedOptions()
+	 * @see \ColdTrick\EntityTools\Migrate::canBackDate()
 	 */
-	protected function setSupportedOptions() {
-		$this->supported_options = [
-			'backdate' => true,
-			'change_owner' => true,
-			'change_container' => true,
-		];
+	public function canBackDate() {
+		return true;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @see \ColdTrick\EntityTools\Migrate::canChangeContainer()
+	 */
+	public function canChangeContainer() {
+		return true;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @see \ColdTrick\EntityTools\Migrate::canChangeOwner()
+	 */
+	public function canChangeOwner() {
+		return true;
 	}
 }
