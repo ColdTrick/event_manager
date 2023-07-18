ce66d526f93e8f66c8c0f0aeda87d2304cb39d06
Jerôme Bakker <jeabakker@coldtrick.com>
2023-07-18 14:01:13 +0200

    fixed: remove unsupported attribute from fieldset

--- views/default/event_manager/maps/osm/location_input.php
+++ views/default/event_manager/maps/osm/location_input.php
@@ -4,6 +4,5 @@ elgg_require_js('event_manager/maps/osm/location_input');
 elgg_load_external_file('css', 'leafletjs');
 
 $field_options = (array) elgg_extract('field_options', $vars);
-$field_options['readonly'] = true;
 
 echo elgg_view_field($field_options);
