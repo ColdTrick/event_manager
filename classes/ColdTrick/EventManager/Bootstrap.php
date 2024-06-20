6e4c65fa22383c0932d7cfeb042089c4a2d624dd
Jerôme Bakker <jeabakker@coldtrick.com>
2024-06-20 13:39:43 +0200

    chore: always get leaflet from https

--- classes/ColdTrick/EventManager/Bootstrap.php
+++ classes/ColdTrick/EventManager/Bootstrap.php
@@ -24,7 +24,7 @@ class Bootstrap extends DefaultPluginBootstrap {
 		}
 		
 		// leafletjs
-		elgg_register_esm('leafletjs', '//unpkg.com/leaflet@1.9.4/dist/leaflet.js');
-		elgg_register_external_file('css', 'leafletjs', '//unpkg.com/leaflet@1.9.4/dist/leaflet.css', 'head');
+		elgg_register_esm('leafletjs', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js');
+		elgg_register_external_file('css', 'leafletjs', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css', 'head');
 	}
 }
